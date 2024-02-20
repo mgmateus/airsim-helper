6fb75a6937b08733bfdb6afb30fbe1e484467aea
mgmateus <eng.mgmateus@gmail.com>
2024-02-20 14:32:57 -0300

    err

--- store.py
+++ store.py
@@ -1,16 +1,40 @@
 import os
+import argparse
+
+import numpy as np
+
+from numpy.typing import NDArray
+
 from simulation import QuadrotorClient
 
 class Storage(QuadrotorClient):
-    def __init__(self, ip : str, vehicle_name : str, camera_name : str, observation : str):
+
+    @staticmethod
+    def process(vertices : NDArray):
+        return vertices.reshape(3, 3)
+
+    def __init__(self, ip : str, vehicle_name : str, camera_name : str, observation : str, path : str):
         super().__init__(ip, vehicle_name, camera_name, observation)
+        self.__path = path
         
     def get_mesh(self, mesh : str):
         for mesh_ in self.simGetMeshPositionVertexBuffers():
             if mesh_.name == mesh:
-                print(mesh_)
+                vertices = np.array(mesh_.vertices)
+                np.save(self.__path + "/"+ mesh_.name, vertices)
+    
         
 if __name__ == "__main__":
-    ip = os.environ['UE4_IP']
-    storage = Storage(ip, 'Hydrone', 'stereo', 'depth')
-    storage.get_meshe('semi_sub')
+    parser = argparse.ArgumentParser(description='Store AirSim Meshes as .npy')
+    parser.add_argument('--ip', type= str, required=True, help='Configurated ip to airsim-ue4 comunication.')
+    parser.add_argument('--path_to_save', type= str, required=True, default=os.environ['UE4_IP'], help='/home/path/to/your/dir')
+    parser.add_argument('--mesh_name', type= int, required=True, help="Number of squares on width in chessboard")
+
+    args = parser.parse_args()
+
+    ip = args.ip
+    storage = Storage(ip, args.path_to_save)
+    storage.get_mesh(args.mesh_name)
+    
+
+    
