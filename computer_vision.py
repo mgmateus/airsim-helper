28f64e94777b4233fb1ab3a874f4ef248f10ded2
mgmateus <eng.mgmateus@gmail.com>
2024-09-25 12:48:43 -0300

    Test camera info

--- computer_vision.py
+++ computer_vision.py
@@ -229,4 +229,8 @@ class ComputerVision(VehicleClient):
 
 if __name__ == "__main__":
     cv = ComputerVision('172.19.0.2')
+
     print(cv.listVehicles())
+    for v in cv.listVehicles():
+        for i in range(6):
+            print(cv.simGetCameraInfo(camera_name=str(i),  vehicle_name=v))
