0330ec6dadfd7c33573f6a39e3b678c31a54070b
mgmateus <eng.mgmateus@gmail.com>
2024-09-25 14:09:10 -0300

    Test camera info

--- computer_vision.py
+++ computer_vision.py
@@ -236,6 +236,7 @@ if __name__ == "__main__":
     
     relative_cp = cv.pose_from_positon_euler_list([0.45, 0, 0.3, 0, 0, 0])
     cv.simSetCameraPose('0',relative_cp)
+    vp, cp = cv.simGetVehiclePose(), cv.simGetCameraInfo('0').pose
     print(f"Vehicle position : {vp.position.to_numpy_array()} -- Camera position : {cp.position.to_numpy_array()}")
 
 
